ac2f70b604fe65bcf1a3a3f68f474dd9e79d5f9e
Guillaume Endignoux <guillaumee@google.com>
2025-10-31 13:18:36 +0100

    Extend CLI example with Rayon and Paralight-on-Rayon backends.

--- examples/cli.rs
+++ examples/cli.rs
@@ -9,11 +9,6 @@
 //! CLI tool to run examples of parallel tasks.
 
 use clap::{Parser, ValueEnum};
-use paralight::iter::{
-    IntoParallelRefMutSource, IntoParallelRefSource, IntoParallelSource, ParallelIteratorExt,
-    ParallelSourceExt, ZipableSource,
-};
-use paralight::{CpuPinningPolicy, RangeStrategy, ThreadCount, ThreadPoolBuilder};
 use rand::seq::index;
 use rand::SeedableRng;
 use rand_chacha::ChaCha12Rng;
@@ -26,6 +21,18 @@ fn main() {
     match cli.parallelism {
         Parallelism::Serial => dispatch_serial(cli),
         Parallelism::Paralight => dispatch_paralight(cli),
+        Parallelism::Rayon => {
+            #[cfg(not(feature = "rayon"))]
+            panic!("Please compile this example with the `rayon` feature to use `--parallelism=rayon`.");
+            #[cfg(feature = "rayon")]
+            dispatch_rayon(cli);
+        }
+        Parallelism::ParalightOnRayon => {
+            #[cfg(not(feature = "rayon"))]
+            panic!("Please compile this example with the `rayon` feature to use `--parallelism=paralight-on-rayon`.");
+            #[cfg(feature = "rayon")]
+            dispatch_paralight_on_rayon(cli);
+        }
     }
 }
 
@@ -137,6 +144,8 @@ fn dispatch_serial(cli: Cli) {
 }
 
 fn dispatch_paralight(cli: Cli) {
+    use paralight::{CpuPinningPolicy, RangeStrategy, ThreadCount, ThreadPoolBuilder};
+
     let mut thread_pool = ThreadPoolBuilder {
         num_threads: match cli.num_threads {
             Some(num_threads) => ThreadCount::Count(num_threads),
@@ -150,12 +159,39 @@ fn dispatch_paralight(cli: Cli) {
     }
     .build();
 
+    dispatch_thread_pool(cli, &mut thread_pool);
+}
+
+#[cfg(feature = "rayon")]
+fn dispatch_paralight_on_rayon(cli: Cli) {
+    use paralight::{RangeStrategy, RayonThreadPool, ThreadCount};
+
+    let thread_pool = RayonThreadPool::new_global(
+        match cli.num_threads {
+            Some(num_threads) => ThreadCount::Count(num_threads),
+            None => ThreadCount::AvailableParallelism,
+        },
+        match cli.range_strategy {
+            RangeStrategyCli::Fixed => RangeStrategy::Fixed,
+            RangeStrategyCli::WorkStealing => RangeStrategy::WorkStealing,
+        },
+    );
+
+    dispatch_thread_pool(cli, &thread_pool);
+}
+
+fn dispatch_thread_pool(cli: Cli, thread_pool: impl paralight::iter::GenericThreadPool) {
+    use paralight::iter::{
+        IntoParallelRefMutSource, IntoParallelRefSource, IntoParallelSource, ParallelIteratorExt,
+        ParallelSourceExt, ZipableSource,
+    };
+
     match (cli.scenario, cli.owned_input, cli.boxed_items) {
         (Scenario::Sum, false, false) => {
             let input = (0..cli.input_size).collect::<Vec<u64>>();
             let sum = black_box(input)
                 .par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .sum::<u64>();
             println!("sum = {sum}");
         }
@@ -163,7 +199,7 @@ fn dispatch_paralight(cli: Cli) {
             let input = (0..cli.input_size).collect::<Vec<u64>>();
             let sum = black_box(input)
                 .into_par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .sum::<u64>();
             println!("sum = {sum}");
         }
@@ -171,7 +207,7 @@ fn dispatch_paralight(cli: Cli) {
             let input = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
             let sum = black_box(input)
                 .par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .map(|x| **x)
                 .sum::<u64>();
             println!("sum = {sum}");
@@ -180,7 +216,7 @@ fn dispatch_paralight(cli: Cli) {
             let input = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
             let sum = black_box(input)
                 .into_par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .map(|x| *x)
                 .sum::<u64>();
             println!("sum = {sum}");
@@ -200,7 +236,7 @@ fn dispatch_paralight(cli: Cli) {
                 black_box(right_slice).par_iter(),
             )
                 .zip_eq()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .for_each(|(out, a, b)| *out = *a + *b);
             println!("added {} elements", black_box(output).len());
         }
@@ -217,7 +253,7 @@ fn dispatch_paralight(cli: Cli) {
                 black_box(right).into_par_iter(),
             )
                 .zip_eq()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .for_each(|(out, a, b)| *out = a + b);
             println!("added {} elements", black_box(output).len());
         }
@@ -236,7 +272,7 @@ fn dispatch_paralight(cli: Cli) {
                 black_box(right_slice).par_iter(),
             )
                 .zip_eq()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .for_each(|(out, a, b)| *out = **a + **b);
             println!("added {} elements", black_box(output).len());
         }
@@ -253,7 +289,7 @@ fn dispatch_paralight(cli: Cli) {
                 black_box(right).into_par_iter(),
             )
                 .zip_eq()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .for_each(|(out, a, b)| *out = *a + *b);
             println!("added {} elements", black_box(output).len());
         }
@@ -262,7 +298,7 @@ fn dispatch_paralight(cli: Cli) {
             let input_slice = input.as_slice();
             let found = black_box(input_slice)
                 .par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_any(|x| **x);
             println!("found = {found:?}");
         }
@@ -270,7 +306,7 @@ fn dispatch_paralight(cli: Cli) {
             let input = fill_needles(cli.input_size as usize, cli.density);
             let found = black_box(input)
                 .into_par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_any(|x| *x);
             println!("found = {found:?}");
         }
@@ -279,7 +315,7 @@ fn dispatch_paralight(cli: Cli) {
             let input_slice = input.as_slice();
             let found = black_box(input_slice)
                 .par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_any(|x| ***x);
             println!("found = {found:?}");
         }
@@ -287,7 +323,7 @@ fn dispatch_paralight(cli: Cli) {
             let input = fill_boxed_needles(cli.input_size as usize, cli.density);
             let found = black_box(input)
                 .into_par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_any(|x| **x);
             println!("found = {found:?}");
         }
@@ -296,7 +332,7 @@ fn dispatch_paralight(cli: Cli) {
             let input_slice = input.as_slice();
             let found = black_box(input_slice)
                 .par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_first(|x| **x);
             println!("found = {found:?}");
         }
@@ -304,7 +340,7 @@ fn dispatch_paralight(cli: Cli) {
             let input = fill_needles(cli.input_size as usize, cli.density);
             let found = black_box(input)
                 .into_par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_first(|x| *x);
             println!("found = {found:?}");
         }
@@ -313,7 +349,7 @@ fn dispatch_paralight(cli: Cli) {
             let input_slice = input.as_slice();
             let found = black_box(input_slice)
                 .par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_first(|x| ***x);
             println!("found = {found:?}");
         }
@@ -321,8 +357,151 @@ fn dispatch_paralight(cli: Cli) {
             let input = fill_boxed_needles(cli.input_size as usize, cli.density);
             let found = black_box(input)
                 .into_par_iter()
-                .with_thread_pool(&mut thread_pool)
+                .with_thread_pool(thread_pool)
                 .find_first(|x| **x);
+            println!("found = {found:?}");
+        }
+    }
+}
+
+#[cfg(feature = "rayon")]
+fn dispatch_rayon(cli: Cli) {
+    use rayon::iter::{
+        IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator,
+        IntoParallelRefMutIterator, ParallelIterator,
+    };
+
+    match (cli.scenario, cli.owned_input, cli.boxed_items) {
+        (Scenario::Sum, false, false) => {
+            let input = (0..cli.input_size).collect::<Vec<u64>>();
+            let sum = black_box(input).par_iter().sum::<u64>();
+            println!("sum = {sum}");
+        }
+        (Scenario::Sum, true, false) => {
+            let input = (0..cli.input_size).collect::<Vec<u64>>();
+            let sum = black_box(input).into_par_iter().sum::<u64>();
+            println!("sum = {sum}");
+        }
+        (Scenario::Sum, false, true) => {
+            let input = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
+            let sum = black_box(input).par_iter().map(|x| **x).sum::<u64>();
+            println!("sum = {sum}");
+        }
+        (Scenario::Sum, true, true) => {
+            let input = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
+            let sum = black_box(input).into_par_iter().map(|x| *x).sum::<u64>();
+            println!("sum = {sum}");
+        }
+        (Scenario::Add, false, false) => {
+            let mut output = vec![0; cli.input_size as usize];
+            let left = (0..cli.input_size).collect::<Vec<u64>>();
+            let right = (0..cli.input_size).collect::<Vec<u64>>();
+
+            let output_slice = output.as_mut_slice();
+            let left_slice = left.as_slice();
+            let right_slice = right.as_slice();
+
+            black_box(output_slice.par_iter_mut())
+                .zip(
+                    black_box(left_slice)
+                        .par_iter()
+                        .zip(black_box(right_slice).par_iter()),
+                )
+                .for_each(|(out, (a, b))| *out = *a + *b);
+            println!("added {} elements", black_box(output).len());
+        }
+        (Scenario::Add, true, false) => {
+            let mut output = vec![0; cli.input_size as usize];
+            let left = (0..cli.input_size).collect::<Vec<u64>>();
+            let right = (0..cli.input_size).collect::<Vec<u64>>();
+
+            let output_slice = output.as_mut_slice();
+
+            black_box(output_slice.par_iter_mut())
+                .zip(
+                    black_box(left)
+                        .into_par_iter()
+                        .zip(black_box(right).into_par_iter()),
+                )
+                .for_each(|(out, (a, b))| *out = a + b);
+            println!("added {} elements", black_box(output).len());
+        }
+        (Scenario::Add, false, true) => {
+            let mut output = vec![0; cli.input_size as usize];
+            let left = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
+            let right = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
+
+            let output_slice = output.as_mut_slice();
+            let left_slice = left.as_slice();
+            let right_slice = right.as_slice();
+
+            black_box(output_slice.par_iter_mut())
+                .zip(
+                    black_box(left_slice)
+                        .par_iter()
+                        .zip(black_box(right_slice).par_iter()),
+                )
+                .for_each(|(out, (a, b))| *out = **a + **b);
+            println!("added {} elements", black_box(output).len());
+        }
+        (Scenario::Add, true, true) => {
+            let mut output = vec![0; cli.input_size as usize];
+            let left = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
+            let right = (0..cli.input_size).map(Box::new).collect::<Vec<Box<u64>>>();
+
+            let output_slice = output.as_mut_slice();
+
+            black_box(output_slice.par_iter_mut())
+                .zip(
+                    black_box(left)
+                        .into_par_iter()
+                        .zip(black_box(right).into_par_iter()),
+                )
+                .for_each(|(out, (a, b))| *out = *a + *b);
+            println!("added {} elements", black_box(output).len());
+        }
+        (Scenario::FindAny, false, false) => {
+            let input = fill_needles(cli.input_size as usize, cli.density);
+            let input_slice = input.as_slice();
+            let found = black_box(input_slice).par_iter().find_any(|x| **x);
+            println!("found = {found:?}");
+        }
+        (Scenario::FindAny, true, false) => {
+            let input = fill_needles(cli.input_size as usize, cli.density);
+            let found = black_box(input).into_par_iter().find_any(|x| *x);
+            println!("found = {found:?}");
+        }
+        (Scenario::FindAny, false, true) => {
+            let input = fill_boxed_needles(cli.input_size as usize, cli.density);
+            let input_slice = input.as_slice();
+            let found = black_box(input_slice).par_iter().find_any(|x| ***x);
+            println!("found = {found:?}");
+        }
+        (Scenario::FindAny, true, true) => {
+            let input = fill_boxed_needles(cli.input_size as usize, cli.density);
+            let found = black_box(input).into_par_iter().find_any(|x| **x);
+            println!("found = {found:?}");
+        }
+        (Scenario::FindFirst, false, false) => {
+            let input = fill_needles(cli.input_size as usize, cli.density);
+            let input_slice = input.as_slice();
+            let found = black_box(input_slice).par_iter().find_first(|x| **x);
+            println!("found = {found:?}");
+        }
+        (Scenario::FindFirst, true, false) => {
+            let input = fill_needles(cli.input_size as usize, cli.density);
+            let found = black_box(input).into_par_iter().find_first(|x| *x);
+            println!("found = {found:?}");
+        }
+        (Scenario::FindFirst, false, true) => {
+            let input = fill_boxed_needles(cli.input_size as usize, cli.density);
+            let input_slice = input.as_slice();
+            let found = black_box(input_slice).par_iter().find_first(|x| ***x);
+            println!("found = {found:?}");
+        }
+        (Scenario::FindFirst, true, true) => {
+            let input = fill_boxed_needles(cli.input_size as usize, cli.density);
+            let found = black_box(input).into_par_iter().find_first(|x| **x);
             println!("found = {found:?}");
         }
     }
@@ -426,4 +605,9 @@ enum Parallelism {
     Serial,
     /// Use paralight.
     Paralight,
+    /// Use rayon. Only available with the `rayon` feature enabled.
+    Rayon,
+    /// Use paralight on top of rayon's global thread pool. Only available with
+    /// the `rayon` feature enabled.
+    ParalightOnRayon,
 }
